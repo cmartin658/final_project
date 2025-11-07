a631bd01393da53c7aba22ff0386ee0f5ed2208c
cmartin658 <carlarkmartin@gmail.com>
2025-11-07 17:27:30 +0000

    finished cleaning data! a table per age group

--- carla_data_cleaning.R
+++ carla_data_cleaning.R
@@ -39,8 +39,11 @@ table_2015_2023 <- A1_4_2015_2023 %>%
          "30-Jun-22" = "...30",
          "30-Jun-23" = "Contents")
 
+# DROP 2015 IN EARLY COLUMN (added after)
+table_2002_2015 <- table_2002_2015 %>%
+  mutate(`30-Jun-15`=NULL)
 
-# SLICING - USED 
+# SLICING - COMPLETE 
 
 early_adults <- table_2002_2015 %>%
   slice(14:23)
@@ -61,7 +64,9 @@ later_15_17 <- table_2015_2023 %>%
   slice(45:57)
 
 
-# PIVOTING - USED
+# PIVOTING 
+
+#ADULTS 
 
 pivot_2002 <- early_adults %>%
   pivot_longer(
@@ -69,6 +74,12 @@ pivot_2002 <- early_adults %>%
     names_to = "date",
     values_to = "numconvicts"
   )
+pivot_2002 <- pivot_2002 %>%
+  pivot_wider(
+    names_from = "Type",
+    values_from = "numconvicts"
+  ) %>%
+  clean_names()
 
 pivot_2015 <- later_adults %>%
   pivot_longer(
@@ -76,14 +87,116 @@ pivot_2015 <- later_adults %>%
     names_to = "date",
     values_to = "numconvicts"
   )
-
 pivot_2015 <- pivot_2015 %>%
   pivot_wider(
     names_from = "Type",
     values_from = "numconvicts"
   )
-  
-pivot_2015 <- pivot_2015 %>%
+
+# 18-20
+# need to sort out 30-jun-15 column as its a character
+# drop because we already have data for 2015
+
+pivot_2002_18_20 <- early_18_20 %>%
+  pivot_longer(
+    cols = c(-"Type"),
+    names_to = "date",
+    values_to = "numconvicts"
+  )
+pivot_2002_18_20 <- pivot_2002_18_20 %>%
+  pivot_wider(
+    names_from = "Type",
+    values_from = "numconvicts"
+  ) %>%
+  clean_names() %>%
+  mutate(x18_20_year_olds = NULL)
+
+pivot_2015_18_20 <- later_18_20 %>%
+  pivot_longer(
+    cols = c(-"Type"),
+    names_to = "date",
+    values_to = "numconvicts"
+  )
+pivot_2015_18_20 <- pivot_2015_18_20 %>%
+  pivot_wider(
+    names_from = "Type",
+    values_from = "numconvicts"
+  ) %>%
   clean_names()
-  mutate(other_offences = criminal_damage_and_arson + possession_of_weapons + public_order_offences + miscellaneous_crimes_against_society + summary_non_motoring) %>%
-  select(Type, violence_against_the_person, sexual_offences, robbery, theft_offences, drug_offences, fraud_offences, summary_motoring, offence_not_recorded)
+
+# 15-17
+
+pivot_2002_15_17 <- early_15_17 %>%
+  pivot_longer(
+    cols = c(-"Type"),
+    names_to = "date",
+    values_to = "numconvicts"
+  )
+pivot_2002_15_17 <- pivot_2002_15_17 %>%
+  pivot_wider(
+    names_from = "Type",
+    values_from = "numconvicts"
+  ) %>%
+  clean_names() %>%
+  mutate(x15_17_year_olds = NULL)
+  
+pivot_2015_15_17 <- later_15_17 %>%
+  pivot_longer(
+    cols = c(-"Type"),
+    names_to = "date",
+    values_to = "numconvicts"
+  )
+pivot_2015_15_17 <- pivot_2015_15_17 %>%
+  pivot_wider(
+    names_from = "Type",
+    values_from = "numconvicts"
+  ) %>%
+  clean_names()
+
+# COMBINE COLUMNS
+
+# ADULTS
+pivot_2015_combined_cols <- pivot_2015 %>%
+  mutate(other_offences = criminal_damage_and_arson + possession_of_weapons + public_order_offences + miscellaneous_crimes_against_society + summary_non_motoring,
+         motoring_offences = summary_non_motoring) %>%
+  select(date, violence_against_the_person, sexual_offences, robbery, theft_offences, drug_offences, fraud_offences, motoring_offences, other_offences, offence_not_recorded) %>%
+  select(order(colnames(pivot_2015_combined_cols)))
+
+pivot_2002_combined_cols <- pivot_2002 %>%
+  mutate(theft_offences = burglary + theft_and_handling,
+         fraud_offences = fraud_and_forgery) %>%
+  select(-c(burglary, theft_and_handling, fraud_and_forgery)) %>%
+  select(order(colnames(pivot_2002_combined_cols)))
+
+# 18-20
+pivot_2015_18_20_combined_cols <- pivot_2015_18_20 %>%
+  mutate(other_offences = criminal_damage_and_arson + possession_of_weapons + public_order_offences + miscellaneous_crimes_against_society + summary_non_motoring,
+         motoring_offences = summary_non_motoring) %>%
+  select(date, violence_against_the_person, sexual_offences, robbery, theft_offences, drug_offences, fraud_offences, motoring_offences, other_offences, offence_not_recorded) 
+
+pivot_2002_18_20_combined_cols <- pivot_2002_18_20 %>%
+  mutate(theft_offences = burglary + theft_and_handling,
+         fraud_offences = fraud_and_forgery) %>%
+  select(-c(burglary, theft_and_handling, fraud_and_forgery))
+
+# 15-17
+pivot_2015_15_17_combined_cols <- pivot_2015_15_17 %>%
+  mutate(other_offences = criminal_damage_and_arson + possession_of_weapons + public_order_offences + miscellaneous_crimes_against_society + summary_non_motoring,
+         motoring_offences = summary_non_motoring) %>%
+  select(date, violence_against_the_person, sexual_offences, robbery, theft_offences, drug_offences, fraud_offences, motoring_offences, other_offences, offence_not_recorded)
+
+pivot_2002_15_17_combined_cols <- pivot_2002_15_17 %>%
+  mutate(theft_offences = burglary + theft_and_handling,
+         fraud_offences = fraud_and_forgery) %>%
+  select(-c(burglary, theft_and_handling, fraud_and_forgery))
+
+# JOIN TABLES 
+
+adults <- full_join(x = pivot_2002_combined_cols,
+          y = pivot_2015_combined_cols)
+
+table_18_20 <- full_join(x = pivot_2002_18_20_combined_cols,
+                         y = pivot_2015_18_20_combined_cols)
+
+table_15_17 <- full_join(x = pivot_2002_15_17_combined_cols,
+                         y = pivot_2015_15_17_combined_cols)
